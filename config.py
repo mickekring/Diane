99783cc9c20032b30a3073f8fc9b5e8ac1a3e287
Micke Kring <jag@mickekring.se>
2023-03-25 20:14:32 +0100

    v 0.2

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
-# version = 0.1
+
 
 ### API KEYS AND CREDENTIALS
 
-OPEN_AI_API_KEY = 'yourapikeyforopenai'
+OPEN_AI_API_KEY = 'sk-sdewfsef9ef98ef2ne8fu893f83uf9n834nf803u0isK9'
+
+GPT3 = "gpt-3.5-turbo"
+GPT4 = "gpt-4"
 
 
 ### NOTES APP
@@ -12,19 +15,34 @@ NOTES_APP = 'obsidian'
 
 ### FILE PATHS
 
-OBSIDIAN_FILE_PATH = '/Users/micke/Obsidian/Micke/Möteanteckningar/'
+OBSIDIAN_FILE_PATH = '/Users/micke/Obsidian/Micke/Möten/'
 
 
 ### TEXT FORMATS - GPT AND DALL-E PROMPT PRIMERS
 
-DALL_E_PROMT_PRIMER = "As a graphic designer, write a prompt for DALL-E that you think matches the \
+YOUR_OWN_PROMTP_PRIMER = "Agera som sekreterare och lyssna efter hur användaren vill att texten ska formatteras och \
+							vilken typ av möte, anteckning eller liknande det handlar om."
+
+
+GENERAL_MEET_PROMPT_PRIMER = "Agera som sekreterare. Sammanställ dessa mötesanteckningar på svenska och formattera texten \
+                              som markdown för Obsidian."
+
+
+TWITTER_PROMPT_PRIMER = "As a social media expert, write an engaging tweet based on the text below in swedish."
+
+
+DALL_E_PROMT_PRIMER = "As a top graphic designer, write a prompt for DALL-E that you think matches the \
 							text below. Avoid using text in the image."
+
 
 LINKED_IN_PROMPT_PRIMER = 'Sammanfatta texten på ett kreativt sätt i form av ett LinkedIn-inlägg på svenska. \
 							Lägg till #-tags om du tycker att det behövs.'
 
-IDEAS_PROMPT_PRIMER = 'Agera som assistent och sammanfatta minnessanteckningarna. Använd ingress \
-							innehållandes det viktigaste först och därefter sammanfattning.'
+
+IDEAS_PROMPT_PRIMER = 'Agera som assistent och sammanfatta minnessanteckningarna på svenska. Använd ingress \
+							innehållandes det viktigaste först och därefter sammanfattning. Texten ska formatteras \
+							som markdown för Obsidian.'
+
 
 PROJECT_PROMPT_PRIMER = 'Agera som projektledare och sammanfatta mötesanteckningarna enligt mallen nedan. \
 						Det ska formatteras som markdown för Obsidian. \nMALL----\n \
